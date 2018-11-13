b53cac9440264314db97d04732a69cd29cfdd868
Inderpreet kaur <inderpreet.b.kaur@capgemini.com>
2018-11-13 18:40:33 +0530

    file

---
+++ HALLO/src/main/java/HALLO/HALLO/App.java
@@ -0,0 +1,13 @@
+package HALLO.HALLO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World2!" );
+    }
+}
